8711363af6d12c24af87d63581143e561c490a1a
Samuel da Rosa Raulino <you64bits@gmail.com>
2025-09-09 11:44:53 -0300

    1/2 fliperama

--- add_game_creator.py
+++ add_game_creator.py
@@ -1,12 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect('banco_games.db')
+# Conectar (ou criar) o banco de dados
+conn = sqlite3.connect('fliper_games.db')
+
+# Criar um cursor para executar comandos SQL
 cursor = conn.cursor()
 
+# Criar a tabela 'usuarios'
 cursor.execute('''
-    INSERT INTO games (nome, adicionado_por)
-    VALUES (?, ?)
-''', ("Doom", "nulo"))
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    arquivo TEXT UNIQUE NOT NULL,
+    width INTEGER,
+    height INTEGER
+)
+''')
+
+# Confirmar as mudanças
 conn.commit()
-print("Usuário inserido com sucesso!")
+
+# Fechar a conexão
 conn.close()
+
+print("Tabela criada com sucesso!")
